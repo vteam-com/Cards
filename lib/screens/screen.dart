66adf206c76ef1f0a09a1233d4a04aa022429fe2
JP Duplessis <jp@vteam.com>
2024-12-06 18:45:31 +0000

    deploy version: 1.5.1

--- lib/screens/screen.dart
+++ lib/screens/screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:share_plus/share_plus.dart';
 
-const version = '1.5.0';
+const version = '1.5.1';
 
 class ResponsiveBreakpoints {
   static const double phone = 600;
